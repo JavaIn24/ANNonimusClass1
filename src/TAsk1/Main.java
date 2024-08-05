51dad1c4ec8cb566ff08b12a2a97f55d4f3d228f
JavaIn24 <yaracrut245@gmail.com>
2024-08-05 09:32:26 +0500

    Update Main.java

--- src/TAsk1/Main.java
+++ src/TAsk1/Main.java
@@ -1,2 +1,28 @@
-package TAsk1;public class Main {
+package TAsk1;
+public class Main {
+   public static void main(String[] args) {
+        Printable print = new Printable() {
+            @Override
+            public void print() {
+                System.out.println("Начал печать на клавиатуре");
+            }
+        };
+
+        Printable print1 = new Printable() {
+            @Override
+            public void print() {
+                System.out.println("Начал рисовать картину");
+            }
+        };
+
+        Printable print2 = new Printable() {
+            @Override
+            public void print() {
+                System.out.println("Написал сочинение");
+            }
+        };
+        print.print();
+        print1.print();
+        print2.print();
+    }
 }
