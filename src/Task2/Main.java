8c36f78e0ba494bf9954a73a1ae309e839ae151a
JavaIn24 <yaracrut245@gmail.com>
2024-08-05 09:33:20 +0500

    Update Main.java

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,2 +1,28 @@
-package Task2;public class Main {
+package Task2;
+public class Main {
+  public static void main(String[] args) {
+        Concationable conCat = new Concationable() {
+            @Override
+            public String concat(String a, String b) {
+                return Concationable.super.concat(a, b);
+            }
+        };
+        Concationable conCat1 = new Concationable() {
+            @Override
+            public String concat(String a, String b) {
+                return Concationable.super.concat(a, b);
+            }
+        };
+        Concationable conCat2 = new Concationable() {
+            @Override
+            public String concat(String a, String b) {
+                return Concationable.super.concat(a, b);
+            }
+        };
+
+        System.out.println(conCat.concat("Василий", "Петров"));
+        System.out.println(conCat1.concat("Сергей", "Няшев"));
+        System.out.println(conCat2.concat("Функциональный", "Интерфейс"));
+
+    }
 }
